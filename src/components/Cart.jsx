2fef0b5ef0cf687b44646add406c551febaa59a1
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-18 12:07:07 +0300

    Create UserProgress context to toggle modal

---
+++ src/components/Cart.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
+import UserProgressContext from "../store/UserProgressContext";
+import Button from "./UI/Button";
+
+const Cart = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from "react";
 import logo from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Header = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  const handleShowCart = () => {
+    userProgressCtx.showCart();
+  };
 
   return (
     <header id="main-header">
@@ -17,7 +23,9 @@ const Header = () => {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>CART ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          CART ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
